70d374cf818f08baa45562eccec43b650fa32c67
Henrique <h.s@uberli.ch>
2019-03-14 22:33:32 +0100

    b

--- index.js
+++ index.js
@@ -44,31 +44,30 @@ shell.exec('npm list -g --depth 1', {silent:true}, function(code, stdout, stderr
 		console.log(stderr);
 	} else {
 		tj.setNPMPackage(stdout);
+
+		let url = getURL();
+		console.log("Connecting to " + url);
+		let socket = require('socket.io-client')(url);
+		// called when connection established
+		socket.on('start', function(data){
+			console.log("connected to " + url + " " + socket.id);
+			console.log(data + " " + (new Date()));
+			socket.emit('checkin', JSON.stringify(tj.getData()));
+		});
+		
+		// called as reply to checkin event with initial config
+		socket.on('init_config', function(data) {
+			let config = JSON.parse(data);
+			tj.configureService(config);
+			console.log("Config set");
+		});
+		
+		// called on browser event
+		socket.on('event', function(data){
+			let param = JSON.parse(data);
+			console.log("event:" + param.target);
+			tj.handleEvent(param);
+		});
 	}
 });
 
-
-
-let url = getURL();
-console.log("Connecting to " + url);
-let socket = require('socket.io-client')(url);
-// called when connection established
-socket.on('start', function(data){
-	console.log("connected to " + url + " " + socket.id);
-	console.log(data + " " + (new Date()));
-	socket.emit('checkin', JSON.stringify(tj.getData()));
-});
-
-// called as reply to checkin event with initial config
-socket.on('init_config', function(data) {
-	let config = JSON.parse(data);
-	tj.configureService(config);
-	console.log("Config set");
-});
-
-// called on browser event
-socket.on('event', function(data){
-	let param = JSON.parse(data);
-	console.log("event:" + param.target);
-	tj.handleEvent(param);
-});
